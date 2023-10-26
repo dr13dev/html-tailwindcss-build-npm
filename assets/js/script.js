45b13abaa0f35699a842b69a92693c190ddb4571
Dipak R <dipakr.developer@gmail.com>
2023-10-26 11:27:39 +0530

    init setup

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+import {$, jQuery} from 'jquery';
+
+var owlCarousel = require('owl.carousel');
+var Lity = require('lity');
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
+});
